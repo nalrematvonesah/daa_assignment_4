e7f1ab172275a688ebb250e1f06ed2947db3a23d
nalrematvonesah <nalrematvonesah@gmail.com>
2025-11-06 14:00:22 +0500

    completed 4th ass

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,24 +21,28 @@ public class Main {
 
         TarjanSCC tarjan = new TarjanSCC();
         List<List<Integer>> sccs = tarjan.findSCCs(g);
-        System.out.println("\nStrongly Connected Components:");
+        int[] compIndex = new int[g.n];
         for (int i = 0; i < sccs.size(); i++) {
-            System.out.println("  SCC " + i + ": " + sccs.get(i));
+            for (int v : sccs.get(i)) compIndex[v] = i;
         }
 
+// Build condensation DAG
         Graph dag = CondensationGraph.buildCondensation(g, sccs);
-        System.out.println("\nCondensation DAG built with " + dag.n + " nodes.");
 
+// Topo order on DAG
         TopologicalSort topo = new TopologicalSort();
         List<Integer> topoOrder = topo.kahnSort(dag);
-        System.out.println("Topological order: " + topoOrder);
+
+// Use the SCC that contains the JSON "source" node
+        int srcNode = data.meta.source;       // 4 from your JSON
+        int srcComp = compIndex[srcNode];     // SCC index containing node 4
 
         DAGShortestPaths sp = new DAGShortestPaths();
+        double[] shortest = sp.shortestPath(dag, srcComp, topoOrder);
+        double[] longest  = sp.longestPath(dag, srcComp, topoOrder);
 
-        double[] shortest = sp.shortestPath(dag, 0, topoOrder);
-        double[] longest = sp.longestPath(dag, 0, topoOrder);
-
-        System.out.println("\nShortest path distances (from SCC 0): " + Arrays.toString(shortest));
-        System.out.println("Longest path distances (from SCC 0): " + Arrays.toString(longest));
+        System.out.println("\nSource node: " + srcNode + " is in SCC " + srcComp);
+        System.out.println("Shortest (from SCC " + srcComp + "): " + Arrays.toString(shortest));
+        System.out.println("Longest  (from SCC " + srcComp + "): " + Arrays.toString(longest));
     }
 }
